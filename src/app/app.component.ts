f1affbfb381628cd0958a0bc53e29765812100d1
Dwaipayan <iamdwaipayan.bhattacharya@gmail.com>
2025-06-29 02:50:11 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'typing-game';
+  randomText:any = lorem.sentence();
+  enteredText:any = '';
+  isMatched:boolean = false;
+
+ngOnInit(): void {
+   this.randomText = lorem.sentence();
+}
+ 
+  onInput(value:String)
+  {
+     this.enteredText = value;
+     this.isMatched = this.enteredText===this.randomText?true:false;
+  }
+
+  onClickReload()
+  {
+     window.location.reload();
+  }
+
+
 }
